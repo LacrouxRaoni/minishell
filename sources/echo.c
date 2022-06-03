b288524f6c6757da5912d511136377a68d4442da
Thome <thomazzribeiro@hotmail.com>
2022-06-03 12:54:06 -0300

    Env + Built-In

---
+++ sources/echo.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tyago-ri <tyago-ri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/23 23:38:14 by tyago-ri          #+#    #+#             */
+/*   Updated: 2022/05/23 23:38:14 by tyago-ri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	echo_built_in(char **cmd)
+{
+	int	i;
+
+	i = 1;
+	if (cmd[1] == NULL)
+		write (1, "\n", 1);
+	else
+	{
+		if (ft_str_check(cmd[1], "-n"))
+			i++;
+		while (cmd[i] != NULL)
+		{
+			if (cmd[i][0] != '\0')
+				ft_putstr_fd(cmd[i], 1);
+			if (cmd[i + 1] != NULL)
+				write (1, " ", 1);
+			i++;
+		}
+		if ((ft_str_check(cmd[1], "-n")) == 0)
+			write (1, "\n", 1);
+	}
+	g_data->mns.exit_code = 0;
+	return ;
+}
---
+++ sources/env.c
@@ -0,0 +1,128 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tyago-ri <tyago-ri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/23 22:06:49 by tyago-ri          #+#    #+#             */
+/*   Updated: 2022/05/23 22:06:49 by tyago-ri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	**cp_first_env(char **env)
+{
+	char	**ret;
+	int		i;
+	int		n;
+
+	i = -1;
+	n = 0;
+	while (env[++i] != NULL)
+	{
+		if (ft_strnstr(env[i], "WORKSPACE", ft_strlen(env[i])) == NULL)
+			n++;
+	}
+	ret = (char **) malloc (sizeof(char *) * (n + 1));
+	n = -1;
+	i = 0;
+	while (env[++n] != NULL)
+	{
+		if (ft_strnstr(env[n], "WORKSPACE", ft_strlen(env[n])) == NULL)
+			ret[i++] = ft_strdup(env[n]);
+	}
+	ret[i] = NULL;
+	return (ret);
+}
+
+char	**copy_env(char **env)
+{
+	char	**ret;
+	int		i;
+	int		n;
+
+	i = 0;
+	n = -1;
+	while (env[i] != NULL)
+		i++;
+	ret = (char **) malloc (sizeof(char *) * (i + 1));
+	while (env[++n] != NULL)
+		ret[n] = ft_strdup(env[n]);
+	ret[n] = NULL;
+	return (ret);
+}
+
+char	*find_env(char *var)
+{
+	int		i;
+
+	i = 0;
+	while (g_data->list.env[i] != NULL)
+	{
+		if (ft_strncmp(var, g_data->list.env[i], ft_strlen(var)) == 0 \
+		&& (g_data->list.env[i][ft_strlen(var)] == '=' \
+		|| g_data->list.env[i][ft_strlen(var)] == '\0'))
+		{
+			if (g_data->list.env[i][ft_strlen(var)] == '=')
+				return (g_data->list.env[i] + ft_strlen(var) + 1);
+			else
+				return ("\0");
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+char	**get_sorted_env(void)
+{
+	int		env_size;
+	char	**env;
+	int		i;
+	char	*temp;
+
+	env = copy_env(g_data->list.env);
+	env_size = 0;
+	while (env[env_size] != NULL)
+		env_size++;
+	while (env_size > 0)
+	{
+		i = 0;
+		while (i < env_size - 1)
+		{
+			if (ft_strncmp(env[i], env[i + 1], ft_strlen(env[i])) > 0)
+			{
+				temp = env[i];
+				env[i] = env[i + 1];
+				env[i + 1] = temp;
+			}
+			i++;
+		}
+		env_size--;
+	}
+	return (env);
+}
+
+void	env_built_in(char **cmd)
+{
+	int	i;
+
+	if (cmd[1] != NULL)
+	{
+		ft_putstr_fd("env: too many arguments\n", STDERR);
+		g_data->mns.exit_code = 1;
+		return ;
+	}
+	else
+	{
+		i = -1;
+		while (g_data->list.env[++i] != NULL)
+		{
+			if (ft_strchr(g_data->list.env[i], '=') != NULL)
+				printf ("%s\n", g_data->list.env[i]);
+		}
+		g_data->mns.exit_code = 0;
+		return ;
+	}
+}
